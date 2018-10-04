39eee51f04fc414b2e24d192b1c6687d84092c1b
cizz3007 <32691294+cizz3007@users.noreply.github.com>
2018-10-04 15:46:38 +0900

    d

--- src/landing/section04/Section04.js
+++ src/landing/section04/Section04.js
@@ -62,8 +62,8 @@ class Section04 extends React.Component {
                 borderRadius: '32px',
                 color: 'rgba(255,255,255,255)',
                 duration: '300',
-                paddingLeft:'15',
-                paddingRight:'15',
+                paddingLeft: '15',
+                paddingRight: '15',
             }).add({
                 targets: nodes,
                 letterSpacing: '0px',
@@ -99,8 +99,8 @@ class Section04 extends React.Component {
                 easing: 'easeInSine',
                 borderRadius: '999px',
                 letterSpacing: '-30px',
-                paddingLeft:'0',
-                paddingRight:'0',
+                paddingLeft: '0',
+                paddingRight: '0',
                 duration: 150,
 
 
@@ -118,6 +118,8 @@ class Section04 extends React.Component {
 
 
     render() {
+        let language = this.props.language;
+
         return (
             <section className={styles['peepee-summary--section']}>
                 <div className={styles['peepee-summary--section--text']}>
@@ -132,21 +134,21 @@ class Section04 extends React.Component {
                             <div
                                 className={cx(styles['__hover-action'])}
                                 onClick={this.clickHandler}>
-                                Sensing unit(Temp. Humid)
+                                {language.section04.second.line1}
                             </div>
                         </a>
                         <a href={'javascript:void(0)'} className={styles['__two']}>
                             <div
                                 className={cx(styles['__hover-action'])}
                                 onClick={this.clickHandler}>
-                                Waterproof Body(IP67)
+                                {language.section04.second.line2}
                             </div>
                         </a>
                         <a href={'javascript:void(0)'} className={styles['__three']}>
                             <div
                                 className={cx(styles['__hover-action'])}
                                 onClick={this.clickHandler}>
-                                Removable battery
+                                {language.section04.second.line3}
                             </div>
                         </a>
                     </div>
@@ -154,30 +156,29 @@ class Section04 extends React.Component {
                         <div className={styles['peepee-spec']}>
                             <img src={require('./con04_01.png')}
                                  alt={'easy to use,Use the supplied patch to apply to any brand your baby likes'}/>
-                            <h2>Easy to Use</h2>
-                            <p>Use the supplied patch to apply<br/>
-                                to any brand your baby likes</p>
+                            <h2>{language.section04.third.line1}</h2>
+                            <p>{language.section04.third.line2}<br/>
+                                {language.section04.third.line3}</p>
                         </div>
                         <div className={styles['peepee-spec']}>
                             <img src={require('./con04_02.png')}
                                  alt={'real-time alerts, Once bowel activity is detected, it will alert you through the app.'}/>
-                            <h2>Real-time Alerts</h2>
-                            <p>Once bowel activity is detected,<br/>
-                                it will alert you through the app.</p>
+                            <h2>{language.section04.third.line4}</h2>
+                            <p>{language.section04.third.line5}<br/>{language.section04.third.line6}</p>
                         </div>
                         <div className={styles['peepee-spec']}>
                             <img src={require('./con04_03.png')}
                                  alt={'reliable data, Keeps track of bowel activities to predict the next one to come.'}/>
-                            <h2>Reliable Data</h2>
-                            <p>Keeps track of bowel activities<br/>
-                                to predict the next one to come.</p>
+                            <h2>{language.section04.third.line7}</h2>
+                            <p>{language.section04.third.line8}<br/>
+                                {language.section04.third.line9}</p>
                         </div>
                         <div className={styles['peepee-spec']}>
                             <img src={require('./con04_04.png')}
                                  alt={'Intelligent Analysis, Accurately predicts bowel activities to reduce the risk of skin rash and infections.'}/>
-                            <h2>Intelligent Analysis</h2>
-                            <p>Accurately predicts bowel activities<br/>
-                                to reduce the risk of skin rash and infections.</p>
+                            <h2>{language.section04.third.line10}</h2>
+                            <p> {language.section04.third.line11}<br/>
+                                {language.section04.third.line12}</p>
                         </div>
                     </div>
                 </div>
